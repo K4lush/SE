d2a5e85b24d49e0b713ef85c4ac5d65dce80b987
Marco <marcofaggiani04@gmail.com>
2023-12-03 01:25:51 +0000

    add class financial_education.py

--- financial_education.py
+++ financial_education.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-def generate_financial_education():
+def display_financial_education():
     # Replace this with your actual logic for financial education
     advice_list = [
         "Create a budget to track your spending.",
@@ -11,16 +11,10 @@ def generate_financial_education():
         "Consider investing for long-term financial goals."
     ]
 
-    return advice_list
-
-def generate_education():
-    # Call the existing function to get financial advice
-    financial_advice = generate_financial_education()
-
     # Display financial advice in the text widget
     education_text.configure(state=tk.NORMAL)
     education_text.delete(1.0, tk.END)
-    for advice in financial_advice:
+    for advice in advice_list:
         education_text.insert(tk.END, f"• {advice}\n")
     education_text.configure(state=tk.DISABLED)
 
@@ -28,13 +22,12 @@ def generate_education():
 root = tk.Tk()
 root.title("Financial Advice")
 
-# Button to generate financial education
-generate_button = ttk.Button(root, text="Generate Financial Advice", command=generate_education)
-generate_button.grid(row=0, column=0, pady=10)
-
 # Text widget to display financial advice
 education_text = tk.Text(root, height=10, width=40, wrap=tk.WORD)
-education_text.grid(row=1, column=0, pady=10)
+education_text.grid(row=0, column=0, pady=10)
+
+# Call the function to display financial advice
+display_financial_education()
 
 # Start the Tkinter event loop
 root.mainloop()
